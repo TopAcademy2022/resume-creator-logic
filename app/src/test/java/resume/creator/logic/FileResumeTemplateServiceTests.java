10d56a400153bfa23ca8f7aff2b4e377120fa073
TopAcademyBel <121047497+TopAcademy2022@users.noreply.github.com>
2025-01-24 20:02:17 +0300

    Added unit testing. Closed #8 (#11)

---
+++ app/src/test/java/resume/creator/logic/FileResumeTemplateServiceTests.java
@@ -0,0 +1,11 @@
+package resume.creator.logic;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class FileResumeTemplateServiceTests {
+    @Test
+    void addFile() {
+        assertTrue(true);
+    }
+}
