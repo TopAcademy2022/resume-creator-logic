e0a67d51822701506a97b2e350a67ae2e95077a1
London3149-train <121044384+London3149-train@users.noreply.github.com>
2025-03-05 20:01:13 +0300

    Updated ResumeService. Closed #22 (#26)

--- app/src/main/java/resume/creator/logic/services/ResumeService.java
+++ app/src/main/java/resume/creator/logic/services/ResumeService.java
@@ -1,6 +1,10 @@
 package resume.creator.logic.services;
 
-import resume.creator.logic.models.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import resume.creator.logic.models.Resume;
+import resume.creator.logic.models.User;
 
 public class ResumeService {
     public boolean AddResume(Resume resume, User user){
@@ -11,5 +15,18 @@ public class ResumeService {
         }
 
         return false;
+    }
+
+    public List<Resume> GetListResume(int user){
+        //List<Resume> resumes = user.Resume;
+        List<Resume> resumes = new ArrayList<Resume>();
+
+        Resume testResume = new Resume();
+        testResume.FIO = "Иванов Илья";
+        testResume.PathToFile = "./myfile.soprano";
+        testResume.PathToResumeTemplate = "./templates/mytemplate.docx";
+        resumes.add(testResume);
+
+        return resumes;
     }
 }
